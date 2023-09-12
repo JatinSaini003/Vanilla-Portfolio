355c85879c21ec86e923abe8435e32f19b26128e
Jatin Saini <jatinkhushi5555@gmail.com>
2023-09-13 00:32:19 +0530

    Vanilla-portfolio

---
+++ Backend.js
@@ -0,0 +1,69 @@
+document.getElementById("skills_box1").addEventListener("mouseover", () => {
+    console.log("hello")
+    document.getElementById("progress_percent1").style.display = 'block';
+});
+
+document.getElementById("skills_box1").addEventListener("mouseout", () => {
+    console.log("hello")
+    document.getElementById("progress_percent1").style.display = 'none';
+});
+
+document.getElementById("skills_box2").addEventListener("mouseover", () => {
+    console.log("hello")
+    document.getElementById("progress_percent2").style.display = 'block';
+});
+
+document.getElementById("skills_box2").addEventListener("mouseout", () => {
+    console.log("hello")
+    document.getElementById("progress_percent2").style.display = 'none';
+});
+
+document.getElementById("skills_box3").addEventListener("mouseover", () => {
+    console.log("hello")
+    document.getElementById("progress_percent3").style.display = 'block';
+});
+
+document.getElementById("skills_box3").addEventListener("mouseout", () => {
+    console.log("hello")
+    document.getElementById("progress_percent3").style.display = 'none';
+});
+
+document.getElementById("skills_box4").addEventListener("mouseover", () => {
+    console.log("hello")
+    document.getElementById("progress_percent4").style.display = 'block';
+});
+
+document.getElementById("skills_box4").addEventListener("mouseout", () => {
+    console.log("hello")
+    document.getElementById("progress_percent4").style.display = 'none';
+});
+
+document.getElementById("skills_box5").addEventListener("mouseover", () => {
+    console.log("hello")
+    document.getElementById("progress_percent5").style.display = 'block';
+});
+
+document.getElementById("skills_box5").addEventListener("mouseout", () => {
+    console.log("hello")
+    document.getElementById("progress_percent5").style.display = 'none';
+});
+
+document.getElementById("skills_box6").addEventListener("mouseover", () => {
+    console.log("hello")
+    document.getElementById("progress_percent6").style.display = 'block';
+});
+
+document.getElementById("skills_box6").addEventListener("mouseout", () => {
+    console.log("hello")
+    document.getElementById("progress_percent6").style.display = 'none';
+});
+
+document.getElementById("skills_box7").addEventListener("mouseover", () => {
+    console.log("hello")
+    document.getElementById("progress_percent7").style.display = 'block';
+});
+
+document.getElementById("skills_box7").addEventListener("mouseout", () => {
+    console.log("hello")
+    document.getElementById("progress_percent7").style.display = 'none';
+});
---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:14-alpine3.16
+
+WORKDIR /the/workdir/path
