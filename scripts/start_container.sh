7fafdd301188142d6c3f194ab130651fef337280
Jatin Saini <107756221+JatinSaini003@users.noreply.github.com>
2024-02-04 16:30:26 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,3 @@
 set -e 
-docker pull jatinsaini003/sample-cicd-project
-docker run -it -p 4000:4000 jatinsaini003/sample-cicd-project
+sudo docker pull jatinsaini003/sample-cicd-project
+sudo docker run -it -p 4000:4000 jatinsaini003/sample-cicd-project
