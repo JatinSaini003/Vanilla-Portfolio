f78a0993793388dcb562cb32a903f9a766c68f37
Jatin Saini <107756221+JatinSaini003@users.noreply.github.com>
2024-02-05 15:18:39 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,2 +1,2 @@
 set -e
-container_id=`sudo docker ps --format "{{.ID}}" | awk -F " " '{print $1}'` && sudo docker rm -f $container_id
+container_id=`sudo docker ps --format "{{.ID}}" | awk -F " " '{print $1}'` && sudo docker rm -f "$container_id"
